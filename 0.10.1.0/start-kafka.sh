9864cf29d026596a2c0252b65a4adbe36ca7099b
bin liu <liubin0329@gmail.com>
2016-11-18 18:19:21 +0800

    make kafka's log to stdout

--- 0.10.1.0/start-kafka.sh
+++ 0.10.1.0/start-kafka.sh
@@ -2,6 +2,4 @@ cd $KAFKA_HOME
 
 nohup bin/zookeeper-server-start.sh config/zookeeper.properties &
 
-nohup bin/kafka-server-start.sh config/server.properties &
-
-tail -f nohup.out
+bin/kafka-server-start.sh config/server.properties
